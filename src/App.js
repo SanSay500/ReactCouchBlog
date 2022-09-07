e9add21784adbc2de1dc72e4e61d419a7d940f43
sansay500 <borodachev@gmail.com>
2022-09-07 18:31:31 +0300

    mmm

--- src/App.js
+++ src/App.js
@@ -1,44 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
 
-
 function App() {
+    var username = 'sansay';
+    var password = 'spartak'
+    const token = `${username}:${password}`;
+    const encodedToken = btoa(token);
+    axios.defaults.baseURL = 'http://localhost:5984';
+    axios.defaults.headers.common['Authorization'] = 'Basic '+ encodedToken;
 
-function fetch_cookie(){
-   axios.post('http://localhost:5984/_session', {
-       name: 'sansay',
-       password: 'spartak'
-   })
-      .then(json => console.log(json))
-}
+        function fetch_dbs() {
+            axios('/_all_dbs')
+                .then(json => console.log(json.data))
+        }
 
-function fetch_session(){
-    axios.get('http://localhost:5984/_session')
-        .then(json => console.log(json))
-}
+        function fetch_database() {
+            axios('/albums/6e1295ed6c29495e54cc05947f18c8af')
+                .then((body) => {
+                    console.log(body.data);
+                });
+        }
+        function add_record(){
+            axios.put('/albums'+axios('/uuids').then(json=>json.data),{
+                name: 'Metal',
+                year: '1995',
+                producer: 'gnome'
+            }).then(json => console.log(json))
+        }
 
-function fetch_dbs(){
-    axios.get('http://localhost:5984/_all_dbs')
-        .then(json => console.log(json))
-}
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <div>
+                        <button onClick={fetch_dbs}>Fetch dbs</button>
+                        <br/>
+                        <button onClick={fetch_database}>Fetch database</button>
+                        <br/>
+                        <button onClick={add_record}>Add record</button>
 
-  return (
-    <div className="App">
-      <header className="App-header">
-        
+                    </div>
+                </header>
+            </div>
+        );
+    }
 
-        <div>
-          <button onClick={fetch_cookie}>Login</button><br/>
-          <button onClick={fetch_session}>Fetch Session</button><br/>
-          <button onClick={fetch_dbs}>Fetch dbs</button>
-
-        </div>
-
-
-      </header>
-    </div>
-  );
-}
-
-export default App;
+    export default App;
